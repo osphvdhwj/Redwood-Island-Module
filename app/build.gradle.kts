c5bec29f13a43fc08ecd4d064ce0658ad050315f
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-03-06 19:49:52 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0"))
 }
