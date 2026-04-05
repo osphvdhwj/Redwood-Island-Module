6cbe56879cf1db59c969af886f837d765319b01c
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-04-05 11:01:33 +0530

    Delete test_brackets.py

--- test_brackets.py
+++
@@ -1,8 +0,0 @@
-with open("head1_config.kt", "r") as f:
-    text = f.read()
-
-count = 0
-for i, char in enumerate(text):
-    if char == '{': count += 1
-    elif char == '}': count -= 1
-print(f"Final Count head1_config: {count}")
