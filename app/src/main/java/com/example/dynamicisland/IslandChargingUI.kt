82cefb79622c730148bb540542b0d69e782b5406
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-04-05 12:58:18 +0530

    Update IslandChargingUI.kt

--- app/src/main/java/com/example/dynamicisland/IslandChargingUI.kt
+++ app/src/main/java/com/example/dynamicisland/IslandChargingUI.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
 fun DynamicIslandView.ChargingMid(charging: LiveActivityModel.Charging) {
