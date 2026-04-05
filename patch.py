0a58e778913bca2f2d2f9c1fa4f966fecea21786
osphvdhwj <manjukhatri0101@gmail.com>
2026-04-05 20:55:06 +0530

    Refactor: Reorganized project structure into sub-packages

--- patch.py
+++
@@ -1,5 +0,0 @@
-with open("app/src/main/java/com/example/dynamicisland/ConfigActivity.kt", "r") as f:
-    lines = f.readlines()
-
-for i in range(165, 175):
-    print(f"{i+1}: {lines[i].rstrip()}")
